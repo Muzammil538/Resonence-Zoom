40f04de24ef4184b729132f65a38cecbfa6da3f6
Muzammil538 <Muzammil538@users.noreply.github.com>
2021-06-06 16:47:03 +0530

    Update
    I have changed the code and made the code lesser.

--- zoom.py
+++ zoom.py
@@ -3,56 +3,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import datetime as dt
 from time import sleep
+import pyttsx3
+#Configuring the browser
 browser = webdriver.Chrome()
-
+browser.maximize_window()
+# Setting the URL of the Website
 url = "https://resonancewarangal.com/liveclasses/home/my_courses"
+Creds = 'JAB212119'
+# This function Logs in to the user's account using the credentials given 
 def LogIn():
     browser.get(url)
     urname = browser.find_element_by_xpath('//*[@id="login-email"]') #Username of mine
     passwd = browser.find_element_by_xpath('/html/body/section[3]/div/div/div/div/div[1]/form/div[1]/div/div[2]/input') #password 
     btn = browser.find_element_by_xpath('/html/body/section[3]/div/div/div/div/div[1]/form/div[2]/button') #Button to login
-    urname.send_keys('JAB212119') #username
-    passwd.send_keys('JAB212119') #password
+    urname.send_keys(Creds) #username
+    passwd.send_keys(Creds) #password
     btn.click() # button is clicked
-# sleep(30)
-def mathsJoin():
+
+
+# Defining the Speak function to reply me
+engine = pyttsx3.init('sapi5')
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def Join():
     LogIn()
     Math_btn = browser.find_element_by_xpath('//*[@id="JoinLink"]').get_attribute("href")
     browser.get(Math_btn)
-    print('Joined Maths')
-def physicsJoin():
-    LogIn()
-    Phy_btn = browser.find_element_by_xpath('//*[@id="JoinLink"]').get_attribute("href")
-    browser.get(Phy_btn)
-    print('Joined Physics')
-def chemJoin():
-    LogIn()
-    Chem_btn = browser.find_element_by_xpath('//*[@id="JoinLink"]').get_attribute("href")
-    browser.get(Chem_btn)
-    print('Joined Chemistry')
-def doubtJoin():
-    LogIn()
-    DT_btn = browser.find_element_by_xpath('//*[@id="JoinLink"]').get_attribute("href")
-    browser.get(DT_btn)
-    print('Joined Math Doubt')
-
-# sleep(30)
-hour = (dt.datetime.now().strftime('%H:%M'))
-# print(type(hour))
-def checkTime():
-        if hour>='8' and hour<='9:15':
-            print("Maths")
-            mathsJoin()
-        elif hour>='9:45' and hour<='11:15':
-            print("Physics")
-            physicsJoin()
-        elif hour>='11:30' and hour<='13':
-            print("Chemistry")
-            chemJoin()
-            # btn2_chem = browser.find_element_by_xpath('//*[@id="JoinLink"]').get_attribute("href")
-            # browser.get(btn2_chem)
-        else:
-            # print("Ended")
-            doubtJoin()
-checkTime()
-    
+    sleep(5)
+    speak('Joined')
+Join()
